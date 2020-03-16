9f4917c87ce32903a6d05fe00f94f220dd28494c
BearBig <bearbig.cn@gmail.com>
2020-03-16 21:45:39 +0800

    使用@Caching组合@Cacheable,@CacheEvict,@CachePut等注解一起使用

--- simple-cache-demo/src/main/java/com/example/bb/defaultcache/dao/StudentDAO.java
+++ simple-cache-demo/src/main/java/com/example/bb/defaultcache/dao/StudentDAO.java
@@ -1,52 +1,65 @@
 package com.example.bb.defaultcache.dao;
 
-import com.example.bb.defaultcache.domain.Student;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.bb.common.dao.AbstractBasicDAO;
+import com.example.bb.common.dao.IStudentDAO;
+import com.example.bb.common.domain.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
+import javax.annotation.Resource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Optional;
 
 /**
+ * Dao的具体实现
+ *
  * @author BB
  * Create: 2020/3/14 9:17
  */
+@Slf4j
 @Component
 public class StudentDAO extends AbstractBasicDAO<Student> implements IStudentDAO {
 
-    @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    private IStudentDAO studentDAO;
+    @Resource
+    private IStudentDAO iStudentDAO;
+
+    public StudentDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
-    @CacheEvict(cacheNames = "students", key = "T(org.springframework.cache.interceptor.SimpleKey).EMPTY")
-    public Optional<Student> insert(Student stu) {
-        int affectedRows = jdbcTemplate.update("insert into student(stu_name, stu_age, stu_birthday) values(?,?,?)", stu.getName(), stu.getAge(), stu.getBirthday());
+    @Caching(
+            cacheable = {@Cacheable(cacheNames = "students", key = "#stu.stuId")},
+            evict = {@CacheEvict(cacheNames = "students", key = "T(org.springframework.cache.interceptor.SimpleKey).EMPTY", beforeInvocation = true)}
+    )
+    public Student insert(Student stu) {
+        int affectedRows = jdbcTemplate.update("insert into student(stu_id, stu_name, stu_age, stu_birthday) values(?,?,?,?)", stu.getStuId(), stu.getName(), stu.getAge(), stu.getBirthday());
         if (affectedRows == 1) {
-            List<Student> students = query("select * from student where stu_name=?", stu.getName());
-            return Optional.ofNullable(students.get(0));
+            Student newStu = queryOne("select * from student where stu_id=?", stu.getStuId());
+            log.info("new student info is {}", newStu);
+            return newStu;
         }
-        return Optional.empty();
+        return null;
     }
 
     @Override
     @Cacheable(cacheNames = "students")
-    public List<Student> selectAll() {
+    public List<Student> selectAllByCache() {
         return query("select * from student");
     }
 
     @Override
     @Cacheable(cacheNames = "students", key = "#id")
-    public Student selectOneByPrimaryKey(Integer id) {
+    public Student selectOneByPrimaryKey(String id) {
         List<Student> students = query("select * from student where stu_id=?", id);
         return CollectionUtils.isEmpty(students) ? null : students.get(0);
     }
@@ -57,27 +70,33 @@ public class StudentDAO extends AbstractBasicDAO<Student> implements IStudentDAO
      * @return 返回所有结果集
      */
     @Override
-    public List<Student> selectAll2() {
+    public List<Student> selectAllNotByCache() {
         // 内部调用此方法，不会缓存
-        return selectAll();
+        return selectAllByCache();
     }
 
     @Override
-    public List<Student> selectAll3() {
+    public List<Student> selectAllByCache2() {
         // 调用此返回，会缓存，因为是spring会通过代理调用
-        return studentDAO.selectAll();
+        return iStudentDAO.selectAllByCache();
     }
 
     @Override
-    @CacheEvict(cacheNames = "students", key = "#id")
-    public boolean deleteByPrimaryKey(Integer id) {
+    @Caching(evict = {
+            @CacheEvict(cacheNames = "students", key = "#id"),
+            @CacheEvict(cacheNames = "students", key = "T(org.springframework.cache.interceptor.SimpleKey).EMPTY")
+    })
+    public boolean deleteByPrimaryKey(String id) {
         int successCount = jdbcTemplate.update("delete from student where stu_id=?", id);
         return successCount == 1;
     }
 
     @Override
-    @CachePut(cacheNames = "students", key = "#id")
-    public Student updateByPrimaryKey(Integer id, String name) {
+    @Caching(
+            put = {@CachePut(cacheNames = "students", key = "#id")},
+            evict = {@CacheEvict(cacheNames = "students", key = "T(org.springframework.cache.interceptor.SimpleKey).EMPTY")}
+    )
+    public Student updateByPrimaryKey(String id, String name) {
         Student stu = null;
         int successCount = jdbcTemplate.update("update student set stu_name=? where stu_id=?", name, id);
         if (successCount == 1) {
@@ -95,7 +114,7 @@ public class StudentDAO extends AbstractBasicDAO<Student> implements IStudentDAO
     @Override
     protected Student generatorE(ResultSet resultSet, int i) throws SQLException {
         return Student.builder()
-                .stuId(resultSet.getInt("stu_id"))
+                .stuId(resultSet.getString("stu_id"))
                 .name(resultSet.getString("stu_name"))
                 .age(resultSet.getInt("stu_age"))
                 .birthday(resultSet.getDate("stu_birthday"))
